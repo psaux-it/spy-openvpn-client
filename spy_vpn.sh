4f9723dd9e5a40e569b7e9c7950126bdad29e1f7
Hasan ÇALIŞIR <hasan.calisir@psauxit.com>
2023-04-26 10:24:56 +0300

    Update spy_vpn.sh

--- spy_vpn.sh
+++ spy_vpn.sh
@@ -76,7 +76,7 @@ fi
 # key-value --> client name-static ip
 clients_name_ip () {
   # declare global associative array
-  declare -A clients
+  declare -gA clients
 
   for each in "${ccd}"/*; do
     if [[ ! -f ${each} ]]; then
